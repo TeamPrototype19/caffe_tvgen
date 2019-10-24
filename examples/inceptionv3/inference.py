f468d62b509389be8add6535447998efe2f2af56
Deokhwan,Kim <thekwan@naver.com>
2019-10-24 18:51:57 +0900

    Change scrip to use not-merged inception V3 prototxt

--- examples/inceptionv3/inference.py
+++ examples/inceptionv3/inference.py
@@ -8,5 +8,5 @@ rimg = pimg.resize((299,299))
 nimga = np.array(rimg).reshape(1,299,299,3).transpose(0,3,1,2)
 
 caffe.set_mode_cpu()
-net = caffe.Net("deploy_inception-v3-merge.prototxt", "inception-v3-merge.caffemodel", 0)
+net = caffe.Net("deploy_inception-v3.prototxt", "inception-v3.caffemodel", 0)
 print net.forward_all(**{"data":nimga})
